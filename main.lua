b9aad4c03b39e50da68da360d02b0eb58924bdf5
Lumi <lumi.f3m@gmail.com>
2026-01-21 07:06:42 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 --[[
-   Hey, Hey, Hey!
+   Hey!
    I am Lumi, the current developer of this Bloxstike script. Give me time to build it. This will be completely open source and free :D
    This project will NOT be Obfuscated and we'll be testing ESP first :D (Check out the ESP.lua file)
 ]]
