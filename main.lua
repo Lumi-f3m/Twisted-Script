60ae7333374574644a47d91b7ff65335d2fe160b
Lumi <lumi.f3m@gmail.com>
2026-01-21 21:30:01 -0500

    Add introductory comment block to main.lua
    Added a comment block introducing the script and its purpose.

--- main.lua
+++ main.lua
@@ -2,4 +2,5 @@
    Hey!
    I am Lumi, the current developer of this Bloxstike script. Give me time to build it. This will be completely open source and free :D
    This project will NOT be Obfuscated and we'll be testing ESP first :D (Check out the ESP.lua file)
+   If you use any component in this script, Please give me some credit atleast. (I wouldn't mind either way)
 ]]
